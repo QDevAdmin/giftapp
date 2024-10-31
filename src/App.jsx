d68bd670e4884a60c8061797abd7ff950d61c69f
kiryu <nikonenko.kirill@wb.ru>
2024-10-31 20:46:57 +0300

    test tg.sendData

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,10 @@ function App() {
             </div>
             <h1>Vite + React</h1>
             <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
+                <button onClick={() => setCount((count) => {
+                    tg.sendData(count);
+                    return count + 1;
+                })}>
                     count is {count}
                 </button>
                 <p>
