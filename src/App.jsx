4c91a86cb1261a2a0557ea4f87ba12e13dd7b55f
kiryu <nikonenko.kirill@wb.ru>
2024-11-01 12:59:55 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import {useEffect} from "react";
 import MainPage from "./components/pages/store/MainPage.jsx";
 
 const App = () => {
+    const tg = window.Telegram.WebApp;
+
+    tg.setHeaderColor('#ffffff');
+    tg.setBackgroundColor('#ffffff');
+    tg.setBottomBarColor('#ffffff');
+
     // disable context menu
     useEffect(() => {
         const handleContextMenu = (e) => e.preventDefault();
